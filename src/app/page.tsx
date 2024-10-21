9bf178b1e55538051899ff149df0085020cb7754
Andrews <andrewschiozo@gmail.com>
2024-10-21 17:03:50 -0300

    feat: estrutura da pagina

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="mx-5">
+      <div className="bg-gray-300 my-2 rounded-md">
+        <h1>Lista de usuários</h1>
+        <div>
+          <button className="p-2 bg-green-300 rounded-md my-2 mx-2">Adicionar</button>
+
+          <ul>
+            <li>User 1</li>
+            <li>User 2</li>
+            <li>User 3</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
